6c9abd4ecf225b68f2f810353b502735684b452e
Scabini Bruno <bscabini@andreani.com>
2024-03-22 11:03:48 -0300

    Proyecto .sln

---
+++ Models/Paciente.cs
@@ -0,0 +1,12 @@
+﻿namespace central_turnos.Models
+{
+    public class Paciente
+    {
+        public int Id { get; set; }
+        public string Apellido { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string DNI { get; set; }
+        public int NroMatricula { get; set; }
+    }
+}
---
+++ Models/Turno.cs
@@ -0,0 +1,11 @@
+﻿}namespace central_turnos.Models
+{
+    public class Turno
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public DateTime Hora { get; set; }
+        public int PacienteId { get; set; }
+        public int MedicoId { get; set; }
+    }
+}
